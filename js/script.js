7a15ece6f9fc88ba329770e7606668894597ac4d
Ross Fenrick <rossfenrick@gmail.com>
2015-03-14 22:20:42 -0500

    Drawer container built

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 $( "#menu" ).click(function() {
 	$(".nav-list").toggleClass("show-menu");
 });
+
+$( ".bus-menu" ).click(function() {
+	$(".drawer").toggleClass("drawer-expand");
+	$(".bus-menu").toggleClass("bus-expand");
+	$(".overlay").toggleClass("overlay-display");
+
+});
