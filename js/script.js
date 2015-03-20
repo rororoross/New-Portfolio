93febff07b1d05f99e3a919bb0ca3d6486a1a945
Ross Fenrick <rossfenrick@gmail.com>
2015-03-19 20:18:25 -0500

    Both the Routes & Drawer Scripts work!

--- js/script.js
+++ js/script.js
@@ -3,13 +3,19 @@ $( "#menu" ).click(function() {
 });
 
 $( ".bus-menu" ).click(function() {
+	$(this).addClass("display-none");
 	$(".drawer").toggleClass("drawer-expand");
-	$(".bus-menu").toggleClass("bus-expand");
 	$(".overlay").toggleClass("overlay-display");
+	
 });
 
-$( "#route-button" ).click(function() {
+$( ".drawer-menu" ).click(function() {
+	$(".drawer").toggleClass("drawer-expand");
+	$(".overlay").toggleClass("overlay-display");
+	$(".bus-menu").removeClass("display-none");
+});
+
+$( ".route-button" ).click(function() {
 	$(".routes").toggleClass("routes-expand");
-	$(".hide-routes").toggleClass("show-route");
-	
+	$(".route-button").toggleClass("routes-button-raise");
 });
