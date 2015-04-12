506c9837cb50f1f7a727a82d929573f92c7883ed
Ross Fenrick <rossfenrick@gmail.com>
2015-04-12 16:45:45 -0500

    Fixed route button being blocked

--- js/script.js
+++ js/script.js
@@ -7,15 +7,17 @@ $( ".bus-menu" ).click(function() {
 	$(".drawer").toggleClass("drawer-expand");
 	$(".overlay").toggleClass("overlay-display");
 	$(".drawer-menu").addClass("display-block");
+	$(".drawer-container").addClass("visible-element");
 });
 
 $( ".drawer-menu" ).click(function() {
 	$(".drawer").toggleClass("drawer-expand");
 	$(".overlay").toggleClass("overlay-display");
 	$(".bus-menu").removeClass("display-none");
+	$(".drawer-container").removeClass("visible-element");
 });
 
-$( ".route-button" ).click(function() {
+$( ".route-button-link" ).click(function() {
 	$(".routes").toggleClass("routes-expand");
 	$(".route-button").toggleClass("routes-button-raise");
 });
