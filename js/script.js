08e0285ad7a23289885c0128cd12f5c63b0c210d
Ross Fenrick <rossfenrick@gmail.com>
2015-04-26 14:26:49 -0500

    Beginning work animations

--- js/script.js
+++ js/script.js
@@ -29,3 +29,23 @@ $( "button" ).click(function() {
 	$(".iphone-container").addClass("fade-in-two");
 	$(".animation-container").addClass("fade-in");
 });
+
+$( ".work-link" ).click(function() {
+	$(".anim").addClass("plz").delay( 600 ).fadeOut( 300 );
+	$(".home").delay( 600 ).fadeOut( 300 );
+	$(".mac-container").addClass("fade-in");
+	$(".iphone-container").addClass("fade-in-two");
+	$(".animation-container").addClass("fade-in");
+});
+
+$( ".mac-container" ).click(function() {
+	$(".mac-container").addClass("work-action-container-fix");
+	$(".iphone-container").addClass("work-action-container-fix");
+	$(".animation-container").addClass("work-action-container-fix");
+	$(".mac").addClass("work-action-mac");
+	$(".mac-link").addClass("work-action-mac");
+	$(this).css("width: auto")
+	$(".iphone").addClass("work-action");
+	$(".animation").addClass("work-action");
+	$("h3").hide();
+});
