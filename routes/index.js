38cbdee4d76545581e6a33a7d4c1fa544f802745
Ross Fenrick <rossfenrick@gmail.com>
2015-10-01 20:34:19 -0500

    Got delay between routes working

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Ross Fenrick | Front End Developer' });
 });
 
 router.get('/my-work', function(req, res, next) {
-  res.render('my-work', { title: 'My Work' });
+  setTimeout((function() {res.render('my-work')}), 840);
 });
 
 	router.get('/work-2', function(req, res, next) {
-	  res.render('work-2', { title: 'More of my work' });
+	 setTimeout((function() {res.render('work-2')}), 840);
 
 });
 
