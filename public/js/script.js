1d181237e81ee360340506409146ffa227286bf9
Ross Fenrick <rossfenrick@gmail.com>
2015-10-01 21:15:39 -0500

    Work page added with animations

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,39 @@
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 600);
+        return false;
+      }
+    }
+  });
+});
+
+$( '.check-out-work' ).click(function() {
+	$('.anim').addClass('plz');
+	$('.home').delay( 600 ).fadeOut( 300 );
+	$('.work').show();
+	$('.mac-container').addClass('fade-in');
+	$('.iphone-container').addClass('fade-in-two');
+	$('.animation-container').addClass('fade-in');
+	console.log('butt stuff');
+});
+
+$( '.work-link' ).click(function() {
+	$('.anim').addClass('plz').delay( 600 ).fadeOut( 300 );
+	$('.home').delay( 600 ).fadeOut( 300 );
+	$('.mac-container').addClass('fade-in');
+	$('.iphone-container').addClass('fade-in-two');
+	$('.animation-container').addClass('fade-in');
+	$('.mobile-container').show();
+});
+
+
+
 $("#butts").click(function(){
 	$(".animate").toggleClass("some-animation");
 });
